a76ad74dd8c8daf7798c2c7639cdfae6ffde0559
salmanausheen55 <snrattamaar@gmail.com>
2023-01-19 23:30:14 +0500

    First commit

---
+++ lib/screens/screen_one.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Screen_one extends StatefulWidget {
+  const Screen_one({super.key});
+
+  @override
+  State<Screen_one> createState() => _Screen_oneState();
+}
+
+class _Screen_oneState extends State<Screen_one> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
